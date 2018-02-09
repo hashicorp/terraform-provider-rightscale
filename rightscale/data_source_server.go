aefc13b7a204b0840ddb50d4c484143f40d9f2a3
Bill <bill.rich@gmail.com>
2018-02-09 11:22:03 -0800

    STEEL-160 Add server data source

---
+++ rightscale/data_source_server.go
@@ -0,0 +1,89 @@
+package rightscale
+
+import (
+	"github.com/hashicorp/terraform/helper/schema"
+	"github.com/rightscale/terraform-provider-rightscale/rightscale/rsc"
+)
+
+func dataSourceServer() *schema.Resource {
+	return &schema.Resource{
+		Read: resourceServerRead,
+
+		Schema: map[string]*schema.Schema{
+			"filter": {
+				Type:     schema.TypeList,
+				MaxItems: 1,
+				Optional: true,
+				ForceNew: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"deployment_href": &schema.Schema{
+							Description: "ID of deployment the server is in",
+							Type:        schema.TypeString,
+							Optional:    true,
+							ForceNew:    true,
+						},
+						"name": &schema.Schema{
+							Description: "name of server",
+							Type:        schema.TypeString,
+							Optional:    true,
+							ForceNew:    true,
+						},
+						"cloud_href": &schema.Schema{
+							Description: "ID of cloud the server is in",
+							Type:        schema.TypeString,
+							Optional:    true,
+							ForceNew:    true,
+						},
+					},
+				},
+			},
+			"description": &schema.Schema{
+				Description: "description of server",
+				Type:        schema.TypeString,
+				Computed:    true,
+			},
+			"instance": &schema.Schema{
+				Description: "server instance details",
+				Type:        schema.TypeList,
+				Elem:        resourceInstance(),
+				Computed:    true,
+			},
+			"name": &schema.Schema{
+				Description: "name of server",
+				Type:        schema.TypeString,
+				Optional:    true,
+				ForceNew:    true,
+			},
+			"optimized": &schema.Schema{
+				Description: "A flag indicating whether Instances of this Server should be optimized for high-performance volumes (e.g. Volumes supporting a specified number of IOPS). Not supported in all Clouds.",
+				Type:        schema.TypeBool,
+				Computed:    true,
+			},
+			"links": {
+				Type:     schema.TypeList,
+				Elem:     &schema.Schema{Type: schema.TypeMap},
+				Computed: true,
+			},
+		},
+	}
+}
+
+func resourceServerRead(d *schema.ResourceData, m interface{}) error {
+	client := m.(rsc.Client)
+	var loc *rsc.Locator
+	loc = &rsc.Locator{Namespace: "rs_cm", Type: "servers"}
+	res, err := client.List(loc, "servers", cmFilters(d))
+	if err != nil {
+		return err
+	}
+
+	if len(res) == 0 {
+		return nil
+	}
+	for k, v := range res[0].Fields {
+		d.Set(k, v)
+	}
+	d.SetId(res[0].Locator.Href)
+	return nil
+}
--- rightscale/provider.go
+++ rightscale/provider.go
@@ -38,6 +38,7 @@ func Provider() terraform.ResourceProvider {
 			"rightscale_multi_cloud_image": dataSourceMultiCloudImage(),
 			"rightscale_network":           dataSourceNetwork(),
 			"rightscale_security_group":    dataSourceSecurityGroup(),
+			"rightscale_server":            dataSourceServer(),
 			"rightscale_server_template":   dataSourceServerTemplate(),
 			"rightscale_ssh_key":           dataSourceSSHKey(),
 			"rightscale_subnet":            dataSourceSubnet(),
